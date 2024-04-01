7bcb2e69af806b09366a308d42917ffdc6444d8a
lauramurinova <lauramurinova@gmail.com>
2024-04-01 12:33:41 -0300

    7_3_Gravity_Manipulation: WIP, added custom objects

--- Assets/Scripts/Plunger.cs
+++ Assets/Scripts/Plunger.cs
@@ -1,7 +1,5 @@
 using System;
-using UnityEditorInternal;
 using UnityEngine;
-using UnityEngine.Assertions;
 
 public class Plunger : MonoBehaviour
 {
@@ -14,23 +12,46 @@ public class Plunger : MonoBehaviour
     public bool active = false;
 
     public GameObject _plunger;
+    public GameObject _sphere;
     
     [SerializeField] private float _force = 10f;
     [SerializeField] private float _radius = 13f;
     [SerializeField] private PlungerType _type = PlungerType.Pull;
     [SerializeField] private LayerMask _plungerInteractionLayers;
 
+    [SerializeField] private MeshRenderer _renderer;
+    [SerializeField] private Material _disabledMat;
+    [SerializeField] private Material _enabledMat;
+
     private Vector3 direction;
     public void Enable(bool enabled)
     {
+        if (enabled)
+        {
+            SetActiveMaterial(_renderer, _enabledMat);
+        }
+        else
+        {
+            SetActiveMaterial(_renderer, _disabledMat);
+        }
         active = enabled;
+    }
+
+    private void SetActiveMaterial(MeshRenderer renderer, Material material)
+    {
+        var materials = renderer.materials;
+        materials[1] = material;
+        renderer.materials = materials;
     }
 
     private void FixedUpdate()
     {
+        
+        
         if(!active) return;
         
         ManipulateRigidbodies(Physics.OverlapSphere(transform.position, _radius, _plungerInteractionLayers));
+        
     }
 
     private void ManipulateRigidbodies(Collider[] colliders)
@@ -39,20 +60,41 @@ public class Plunger : MonoBehaviour
         {
             if(!collider.TryGetComponent(out Rigidbody rigidbody)) continue;
 
-            rigidbody.velocity *= 0.8f;
+            rigidbody.velocity *= 0.5f;
+            rigidbody.useGravity = false;
             if (IsAnotherPlungerInAreaActive())
             {
-               //TODO
+                // rigidbody.velocity *= 0f;
+                // Vector3 newPosition = ((transform.position + _plunger.transform.position) / 2f) + transform.forward;
+                // direction = newPosition - collider.transform.position;
+                // rigidbody.transform.position = newPosition;
+                
+                if (_type.Equals(PlungerType.Pull))
+                {
+                    Vector3 newPosition = ((transform.position + _plunger.transform.position) / 2f) + transform.forward * 3f;
+                    direction = newPosition - collider.transform.position;
+                    rigidbody.AddForce(direction.normalized * (_force), ForceMode.Acceleration);
+                }
+                else if (_type.Equals(PlungerType.Push))
+                {
+                    Vector3 newPosition = transform.position + transform.forward * 3f;
+                    direction = (newPosition - (transform.position));
+                    rigidbody.AddForce(-direction.normalized * (_force), ForceMode.Acceleration);
+                }
             }
-            else if (_type.Equals(PlungerType.Push))
+            else
             {
-                direction = (transform.position - collider.transform.position);
-                rigidbody.AddForce(-direction.normalized * (_force), ForceMode.VelocityChange);
-            }
-            else if(_type.Equals(PlungerType.Pull))
-            {
-                direction = (transform.position - collider.transform.position);
-                rigidbody.AddForce(direction.normalized * (_force), ForceMode.VelocityChange);
+                rigidbody.useGravity = true;
+                if (_type.Equals(PlungerType.Pull))
+                {
+                    direction = transform.position - collider.transform.position;
+                    rigidbody.AddForce(direction.normalized * (_force), ForceMode.Acceleration);
+                }
+                else if (_type.Equals(PlungerType.Push))
+                {
+                    direction = (collider.transform.position - transform.position);
+                    rigidbody.AddForce(direction.normalized * (_force), ForceMode.Acceleration);
+                }
             }
         }
     }
@@ -75,6 +117,8 @@ public class Plunger : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawLine(transform.position, _plunger.transform.position);
+        
+        Vector3 newPosition = ((transform.position + _plunger.transform.position) / 2f) + transform.forward;
+        Gizmos.DrawLine(transform.position, newPosition);
     }
 }
