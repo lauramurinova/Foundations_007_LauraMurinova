b2da5c4b497007be47c67194cb2bea0bf50c483b
lauramurinova <lauramurinova@gmail.com>
2024-04-09 16:27:34 -0300

    8_3_Control_Volume: Finished assignment 3 of week 8

--- Assets/Scripts/XR/ControlVolume.cs
+++ Assets/Scripts/XR/ControlVolume.cs
@@ -1,136 +1,120 @@
-using System;
-using System.Collections;
 using UnityEngine;
 using UnityEngine.Assertions;
+using UnityEngine.XR.Interaction.Toolkit;
 
-public class ControlVolume : MonoBehaviour
+public class ControlVolume : XRBaseInteractable
 {
-    public float movementSpeed = 1f;
-    public float rotationSpeed = 10f;
-    
     [SerializeField] private Rigidbody _interactable;
+    [SerializeField] private Transform _rotationReference;
+    [SerializeField] private float _movementForce = 5f;
+    [SerializeField] private float _rotationSpeed = 5f;
+    
     [SerializeField] private Color _disabledColor;
     [SerializeField] private Color _enabledColor;
 
-    private ControlVolumeInput[] _controlVolumeInputs;
     private bool _active = false;
     private Rigidbody _interactor;
     private MeshRenderer _renderer;
     private Vector3 _previousHandPosition;
-    private Quaternion _previousRotation;
-    private bool _isRotating = false;
 
     private void Start()
     {
         Assert.IsNotNull(_interactable, "Have not assigned interactable on Control Volume " + name);
-
-        _controlVolumeInputs = GameObject.FindObjectsOfType<ControlVolumeInput>();
-        foreach (var controlVolume in _controlVolumeInputs)
-        {
-            controlVolume.controlVolumePressed.AddListener(Activate);
-            controlVolume.controlVolumeReleased.AddListener(Deactivate);
-        }
+        Assert.IsNotNull(_rotationReference, "Have not assigned rotation reference on Control Volume " + name);
 
         _renderer = GetComponent<MeshRenderer>();
     }
 
-    public void Deactivate()
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
-        _active = false;
-        _interactor = null;
-        _renderer.material.color = _disabledColor;
-        _interactable.isKinematic = true;
-        _interactable.useGravity = true;
-        StopAllCoroutines();
-        _isRotating = false;
-    }
-
-    public void Activate()
-    {
+        // activate the control volume
+        EnableInteractable();
+        _rotationReference.SetParent(_interactor.transform);
         _active = true;
+        _previousHandPosition = _interactor.transform.position;
+        _renderer.material.color = _enabledColor;
     }
 
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        // deactivate the control volume
+        _active = false;
+        _renderer.material.color = _disabledColor;
+        DisableInteractable();
+        _rotationReference.SetParent(transform);
+    }
     private void FixedUpdate()
     {
-        if(!_interactor) return;
-        
-         _interactable.rotation = Quaternion.Slerp(_interactable.rotation, _interactor.transform.rotation, rotationSpeed * Time.deltaTime);
-            
-                Vector3 handMovement = _interactor.transform.position - _previousHandPosition;
+        if(!_interactor || !_active) return;
 
-            float forwardDot = Vector3.Dot(handMovement.normalized, _interactor.transform.forward);
-            float backwardDot = Vector3.Dot(handMovement.normalized, -_interactor.transform.forward);
-            float rightDot = Vector3.Dot(handMovement.normalized, _interactor.transform.up);
-            float leftDot = Vector3.Dot(handMovement.normalized, -_interactor.transform.up);
-            float upDot = Vector3.Dot(handMovement.normalized, _interactor.transform.right);
-            float downDot = Vector3.Dot(handMovement.normalized, -_interactor.transform.right);
-
-            if (_interactor.velocity.magnitude > 0.05f)
-            {
-                if (forwardDot > 0.8f)
-                {
-                    _interactable.velocity *= 0.9f;
-                    _interactable.AddForce(_interactor.transform.forward * (handMovement.magnitude * 800f),
-                        ForceMode.Acceleration);
-                }
-                else if (backwardDot > 0.8f)
-                {
-                    _interactable.velocity *= 0.9f;
-                    _interactable.AddForce(-_interactor.transform.forward * (800f * handMovement.magnitude),
-                        ForceMode.Acceleration);
-                }
-                else if (rightDot > 0.8f)
-                {
-                    _interactable.velocity *= 0.9f;
-                    _interactable.AddForce(_interactor.transform.up * (800f * handMovement.magnitude),
-                        ForceMode.Acceleration);
-                }
-                else if (leftDot > 0.8f)
-                {
-                    _interactable.velocity *= 0.9f;
-                    _interactable.AddForce(-_interactor.transform.up * (800f * handMovement.magnitude),
-                        ForceMode.Acceleration);
-                }
-                else if (upDot > 0.8f)
-                {
-                    _interactable.velocity *= 0.9f;
-                    _interactable.AddForce(_interactor.transform.right * (800f * handMovement.magnitude),
-                        ForceMode.Acceleration);
-                }
-                else if (downDot > 0.8f)
-                {
-                    _interactable.velocity *= 0.9f;
-                    _interactable.AddForce(-_interactor.transform.right * (800f * handMovement.magnitude),
-                        ForceMode.Acceleration);
-                }
-            }
-            else
-            {
-                _interactable.velocity *= 0.55f;
-            }
-            
-            _previousHandPosition = _interactor.transform.position;
-            _renderer.material.color = _enabledColor;
+        HandleRotation();
+        HandleMovement();
     }
 
-    private void OnTriggerStay(Collider other)
+    /// <summary>
+    /// Handles smooth movement of the assigned interactable based on the interactor movement in the volume.
+    /// </summary>
+    private void HandleMovement()
     {
-        if (other.gameObject.layer.Equals(LayerMask.NameToLayer("Hand")) && _active)
-        {
+        Vector3 handMovement = _interactor.transform.position - _previousHandPosition;
+        _interactable.velocity += handMovement * (_movementForce + (handMovement.magnitude*10));
+        _previousHandPosition = _interactor.transform.position;
+    }
 
+    /// <summary>
+    /// Handles smooth rotation of the assigned interactable based on the rotation reference.
+    /// </summary>
+    private void HandleRotation()
+    {
+        var targetRotation = Quaternion.LookRotation(_rotationReference.forward, _rotationReference.up);
+        _interactable.rotation = Quaternion.Slerp(_interactable.rotation, targetRotation, _rotationSpeed * Time.fixedDeltaTime);
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer.Equals(LayerMask.NameToLayer("Hand")))
+        {
             if (_interactor == null)
             {
+                // save interactor (hand) reference
                 _interactor = other.attachedRigidbody;
-                _interactable.isKinematic = false;
-                _interactable.useGravity = false;
-                _previousHandPosition = _interactor.transform.position;
-                _previousRotation = _interactor.transform.rotation;
+                
+                // if its active (user is still pressing the button), activate the volume interactions
+                if (_active)
+                {
+                    _renderer.material.color = _enabledColor;
+                    EnableInteractable();
+                }
             }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        Deactivate();
+        // if the interactor (hand) left the volume, disable interactions
+        if (other.attachedRigidbody == _interactor)
+        {
+            _interactor = null;
+            _renderer.material.color = _disabledColor;
+            DisableInteractable();
+        }
+    }
+    
+    /// <summary>
+    /// Enables physics on the assigned interactable.
+    /// </summary>
+    private void EnableInteractable()
+    {
+        _interactable.isKinematic = false;
+        _interactable.useGravity = false;
+    }
+
+    /// <summary>
+    /// Disables physics on the assigned interactable.
+    /// </summary>
+    private void DisableInteractable()
+    {
+        _interactable.isKinematic = true;
+        _interactable.useGravity = true;
     }
 }
